1f683f86272cf3e570d22dbad26cc535ed78f84a
quocthai027 <113010699+quocthai027@users.noreply.github.com>
2022-11-08 23:14:58 +0700

    aa

---
+++ flutter_application_1/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\flutter_windows_3.3.1-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\quoct\Desktop\didongbuoi3\thuchanhbuoi3\Layout\left_side\Buoi6\phan2_bai1\0306201077_LeQuocThai_phan3\phan4_bai1\Git\DoAn\flutter_application_1"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
